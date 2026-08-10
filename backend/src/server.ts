39e3ea85ee074c4d452fec35dddf7863c6b0097d
Saad Sayyed <saadsyed950@gmail.com>
2026-08-10 14:16:09 +0530

    System health

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import { env } from "./config/env.config";
 
@@ -7,6 +7,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", (_req: Request, res: Response) => {
+  try {
+    res.status(200).json({ health: "OK", status: 200 });
+  } catch (error: any) {
+    return res.status(500).json({ health: "BAD", status: 500 });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);

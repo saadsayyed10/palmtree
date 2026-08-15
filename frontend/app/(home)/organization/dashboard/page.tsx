df43e0e098e4220f5ba4db425d4c173477da7e8c
Saad Sayyed <saadsyed950@gmail.com>
2026-08-15 23:25:14 +0530

    Disapproved Dashboard: UI

--- frontend/app/(home)/organization/dashboard/page.tsx
+++ frontend/app/(home)/organization/dashboard/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
-import { ArrowBigRight, Loader2, Rocket } from "lucide-react";
+import {
+  ArrowRight,
+  ChartColumnStacked,
+  FolderKanban,
+  Handshake,
+  IdCardLanyard,
+  Loader2,
+  Lock,
+  Rocket,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -48,7 +58,7 @@ const OrganizationDashboard = () => {
               className={"lg:p-6 text-blue-700"}
               onClick={() => router.push("/organization/setup")}
             >
-              Set Up Organization <ArrowBigRight />
+              Set Up Organization <ArrowRight />
             </Button>
 
             <Button
@@ -74,7 +84,90 @@ const OrganizationDashboard = () => {
         </div>
       )}
 
-      <div>Hi</div>
+      <div className="flex justify-start items-start w-full lg:gap-x-10">
+        <div className="border shadow-lg rounded-xl w-100 h-60 bg-white flex justify-start items-start flex-col lg:gap-y-4 lg:p-4">
+          <div className="flex justify-between items-center w-full">
+            <Button size={"lg"} variant={"secondary"} disabled>
+              <IdCardLanyard />
+            </Button>
+
+            <Badge>
+              <Lock /> Locked
+            </Badge>
+          </div>
+
+          <div className="flex justify-start items-start flex-col lg:gap-y-2">
+            <h3 className="text-2xl font-semibold text-neutral-800">
+              Employees
+            </h3>
+
+            <p className="text-neutral-400 text-sm">
+              View and manage your internal workforce, roles and hierarchies.
+            </p>
+          </div>
+        </div>
+
+        <div className="border shadow-lg rounded-xl w-100 h-60 bg-white flex justify-start items-start flex-col lg:gap-y-4 lg:p-4">
+          <div className="flex justify-between items-center w-full">
+            <Button size={"lg"} variant={"secondary"} disabled>
+              <FolderKanban />
+            </Button>
+
+            <Badge>
+              <Lock /> Locked
+            </Badge>
+          </div>
+
+          <div className="flex justify-start items-start flex-col lg:gap-y-2">
+            <h3 className="text-2xl font-semibold text-neutral-800">
+              Projects
+            </h3>
+
+            <p className="text-neutral-400 text-sm">
+              Track roadmap progress, budget allocations and cross-team
+              delivery.
+            </p>
+          </div>
+        </div>
+
+        <div className="border shadow-lg rounded-xl w-100 h-60 bg-white flex justify-start items-start flex-col lg:gap-y-4 lg:p-4">
+          <div className="flex justify-between items-center w-full">
+            <Button size={"lg"} variant={"secondary"} disabled>
+              <Handshake />
+            </Button>
+
+            <Badge>
+              <Lock /> Locked
+            </Badge>
+          </div>
+
+          <div className="flex justify-start items-start flex-col lg:gap-y-2">
+            <h3 className="text-2xl font-semibold text-neutral-800">
+              Service Providers
+            </h3>
+
+            <p className="text-neutral-400 text-sm">
+              Manage external vendors, contracts and specialized service
+              agreements.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center w-full flex-col lg:gap-y-1 lg:mt-10 text-center">
+        <span className="p-4 bg-neutral-100 rounded-full">
+          <ChartColumnStacked className="w-10 h-10 stroke-2 text-neutral-400" />
+        </span>
+
+        <h4 className="text-xl font-semibold text-neutral-800 mt-6">
+          No data to display
+        </h4>
+
+        <h6 className="text-sm text-neutral-400">
+          Analytics and reporting will appear here once your
+          <br /> organization is fully configured and approved.
+        </h6>
+      </div>
     </div>
   );
 };
